6cda6a56f99d5479fac340f9725dcd4553ffe2b1
Pramesti Hatta K <opam.qweiop@gmail.com>
2017-06-23 01:09:33 +0700

    move store

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-import reducer from './reducers';
+import reducer from '../reducers';
 
 const middlewares = [];
 
@@ -23,5 +23,3 @@ function configureStore(initialState) {
 const store = configureStore({});
 
 export default store;
-
-
